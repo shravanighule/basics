8dfd799da39bfd07c4c1dbb0f8e04ea3caee13b0
shravanighule <shravanighule16@gmail.com>
2024-08-27 10:43:47 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+//Name - Shravani Ghule
+//PRN - 23070123056
+//Aim - Simple calculator
+#include <iostream>
+using namespace std;
+
+int main() {   
+    float num1,num2;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "The sum is: " << num1+num2 << "\n";;
+    cout << "The difference is: " << num1-num2 << "\n";;
+    cout << "The division is: " << num1/num2 << "\n";;
+    cout << "The product is: " << num1*num2 << "\n";;
+
+    return 0;
+}
+/* Output:
+Enter num1: 5
+Enter num2: 1
+The sum is: 6
+The difference is: 4
+The division is: 5
+The product is: 5
+*/
